9b8edab1d993d1105dea635e9e3ac1e6d79639e2
na50r <shanrosan@gmail.com>
2024-01-22 12:45:21 +0100

    Dabbled my fingers in C and JS

---
+++ C/FileToArray.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+char words1[][20] = {
+    "Hello",
+    "Hi",
+    "Fuck You"
+};
+
+int main(void){
+    printf("Penis\n");
+    for(int i = 0; i<3; i++){
+        printf("%s\n", words1[i]);
+    }
+}
+
