01a787c1c45a5d360120475e4a7f26cae89c6f3d
Rosan Shanmuganathan <rosan.shanmuganathan@uzh.ch>
2024-02-05 17:44:54 +0100

    Worked more on React App

--- React/Basics/react-app/src/App.tsx
+++ React/Basics/react-app/src/App.tsx
@@ -1,8 +1,24 @@
 import ListGroup from "./components/ListGroup";
 import Message from "./Message";
+import Alert from "./components/Alert";
 
-function App(){
-  return <div><ListGroup/></div>;
+// function App(){
+//   const handleSelectItem = (item:string)=>{
+//     console.log(item);
+//   }
+//   let items = ["New York", "San Francisco", "Los Angeles", "Las Vegas", "London", "Manchester"];
+
+//   return <div><ListGroup items={items} heading='Cities' onSelectItem={handleSelectItem}/></div>;
+// }
+
+// export default App;
+
+function App() {
+  return (
+    <div>
+      <Alert text="Hello World"/>
+    </div>
+  );
 }
 
 export default App;
--- React/Basics/react-app/src/components/ListGroup.tsx
+++ React/Basics/react-app/src/components/ListGroup.tsx
@@ -1,11 +1,17 @@
 import { useState } from "react";
+//{items:[], heading:string}
+
+interface Props {
+  items: string[];
+  heading: string;
+  //(item:string)=>void
+  onSelectItem: (item: string) => void;
+}
 
 // ListGroup.tsx
-function ListGroup() {
-  let items = ["Jason", "Mary", "Kelsy", "Francis", "Kai"];
+function ListGroup({ items, heading, onSelectItem }: Props) {
   //Hook
   const [selectedIndex, setSelectedIndex] = useState(-1);
-
 
   const getMsg = () => {
     return items.length === 0 ? <p>No item found</p> : null;
@@ -13,14 +19,21 @@ function ListGroup() {
 
   return (
     <>
-      <h1>List</h1>
+      <h1>{heading}</h1>
       {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
         {items.map((item, index) => (
           <li
-            className={selectedIndex === index ? 'list-group-item active': 'list-group-item'}
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
             key={item}
-            onClick={()=>{setSelectedIndex(index);}}
+            onClick={() => {
+              setSelectedIndex(index);
+              onSelectItem(item);
+            }}
           >
             {item}
           </li>
